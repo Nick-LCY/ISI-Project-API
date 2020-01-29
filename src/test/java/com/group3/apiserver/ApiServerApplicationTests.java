5ae9723268144de142367be1659275a630e53c6d
Nick_Lin <nicklin9907@gmail.com>
2020-01-29 13:43:03 +0800

    project init

---
+++ src/test/java/com/group3/apiserver/ApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group3.apiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
