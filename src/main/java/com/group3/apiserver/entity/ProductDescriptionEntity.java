1341eae1efb6dbb6dad89bc7de8d42d58f87d3b1
Nick_Lin <nicklin9907@gmail.com>
2020-02-11 18:29:34 +0800

    forget to commit productDescriptionEntity

---
+++ src/main/java/com/group3/apiserver/entity/ProductDescriptionEntity.java
@@ -0,0 +1,85 @@
+package com.group3.apiserver.entity;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@Entity
+@Table(name = "product_description", schema = "isi")
+public class ProductDescriptionEntity {
+    private int id;
+    private String attributeName;
+    private String attributeValue;
+    private int sequence;
+    private int productId;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "attribute_name")
+    public String getAttributeName() {
+        return attributeName;
+    }
+
+    public void setAttributeName(String attributeName) {
+        this.attributeName = attributeName;
+    }
+
+    @Basic
+    @Column(name = "attribute_value")
+    public String getAttributeValue() {
+        return attributeValue;
+    }
+
+    public void setAttributeValue(String attributeValue) {
+        this.attributeValue = attributeValue;
+    }
+
+    @Basic
+    @Column(name = "sequence")
+    public int getSequence() {
+        return sequence;
+    }
+
+    public void setSequence(int sequence) {
+        this.sequence = sequence;
+    }
+
+    @Basic
+    @Column(name = "product_id")
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductDescriptionEntity that = (ProductDescriptionEntity) o;
+        return id == that.id &&
+                sequence == that.sequence &&
+                productId == that.productId &&
+                Objects.equals(attributeName, that.attributeName) &&
+                Objects.equals(attributeValue, that.attributeValue);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, attributeName, attributeValue, sequence, productId);
+    }
+}
